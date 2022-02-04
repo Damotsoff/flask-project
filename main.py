9a9ab311d778f6d4ee4ae4a8aa9690a274559bf4
George Damotsov <george.damotsov@gmail.com>
2022-02-04 21:15:07 +0300

    вывод результата

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 from crypt import methods
 from distutils.log import debug
+from unittest import result
 from flask import Flask,render_template,request
 from search import search4letters
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello() -> str:
     return 'Hello world'
 
+
 @app.route('/search4',methods=['POST'])
-def do_search() -> str:
+def do_search() -> 'html':
     phrase =request.form['phrase']
     letters =request.form['letters']
-    return str(search4letters(phrase,letters))
+    title = 'Here are your result'
+    results = str(search4letters(phrase,letters))
+    return render_template('result.html',the_phrase =phrase,the_letters = letters,\
+        the_results =results,the_title =title)
 
 
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title = 'Welcome to Search Leaters on the Web')
+
 
 app.run(debug=True)
