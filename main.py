b9d495d94e4bf1bc63c7d99c7026576aa1ebd7f6
George Damotsov <george.damotsov@gmail.com>
2022-02-05 15:29:21 +0300

    logging update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 from search import search4letters
 
 
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 def log_request(req: 'flask_request', res: str) -> None:
     with open('vsearch.log', 'a') as log:
-        print(req, res, file=log)
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
 
 
 @app.route('/search4', methods=['POST'])
@@ -28,6 +28,17 @@ def do_search() -> 'html':
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome to Search Leaters on the Web')
+
+
+@app.route('/viewlog')
+def view_log() -> str:
+    contents = []
+    with open('vsearch.log', 'r') as log:
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    return str(contents)
 
 
 print('We start off in:', __name__)
