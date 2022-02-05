19d352b54e5d55946f4479d5cc02b8b50ec54eef
George Damotsov <george.damotsov@gmail.com>
2022-02-05 14:33:45 +0300

    logging

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 from search import search4letters
 
 
 app = Flask(__name__)
 
 
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log', 'a') as log:
+        print(req, res, file=log)
 
 
-
-@app.route('/search4',methods=['POST'])
+@app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
-    phrase =request.form['phrase']
-    letters =request.form['letters']
+    phrase = request.form['phrase']
+    letters = request.form['letters']
     title = 'Here are your result'
-    results = str(search4letters(phrase,letters))
+    results = str(search4letters(phrase, letters))
+    log_request(request, results)
     return render_template('result.html',
-                            the_phrase =phrase,
-                            the_letters = letters,
-                            the_results =results,
-                            the_title =title)
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_results=results,
+                           the_title=title)
+
 
 @app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
-    return render_template('entry.html', the_title = 'Welcome to Search Leaters on the Web')
+    return render_template('entry.html', the_title='Welcome to Search Leaters on the Web')
 
-if __name__=='__main__':
+
+print('We start off in:', __name__)
+if __name__ == '__main__':
     app.run(debug=True)
