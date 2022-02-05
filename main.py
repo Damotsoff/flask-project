83c3d564292f13741b376085c94b659a94209a73
George Damotsov <george.damotsov@gmail.com>
2022-02-05 17:11:02 +0300

    add database

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, escape
 from search import search4letters
+import mysql.connector
 
 
 app = Flask(__name__)
@@ -8,6 +9,25 @@ app = Flask(__name__)
 def log_request(req: 'flask_request', res: str) -> None:
     with open('vsearch.log', 'a') as log:
         print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+        dbconfig = {
+            'host': '127.0.0.1',
+            'user': 'vsearch',
+            'password': 'vsearchpasswd',
+            'database': 'vsearchlogDB'
+        }
+        conn = mysql.connector.connect(**dbconfig)
+        cursor = conn.cursor()
+        _SQL = """insert into log
+                    (phrase, letters, ip, browser_string, result)
+                    values(%s, %s, %s, %s, %s)"""
+        cursor.execute(_SQL, (req.form['phrase'],
+                              req.form['letters'],
+                              req.remote_addr,
+                              req.user_agent.browser,
+                              res, ))
+        conn.commit()
+        cursor.close()
+        conn.close()
 
 
 @app.route('/search4', methods=['POST'])
