ceacdfecd2a2d44f4368f7280fdcef49d59df9e1
George Damotsov <george.damotsov@gmail.com>
2022-02-06 00:42:33 +0300

    подключил поток

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, escape
+from threading import Thread
+from flask import Flask, render_template, request, escape, session, copy_current_request_context
 from search import search4letters
-from dbcm import UseDatabase
+from dbcm import UseDatabase, ConnectionError, CredentialsError, SQLError
 from cheker import check_logged_in
 
 
@@ -13,25 +14,41 @@ app.config['dbconfig'] = {
 }
 
 
-def log_request(req: 'flask_request', res: str) -> None:
-    with UseDatabase(app.config['dbconfig']) as cursor:
-        _SQL = """insert into log
-                    (phrase, letters, ip, browser_string, result)
-                    values(%s, %s, %s, %s, %s)"""
-        cursor.execute(_SQL, (req.form['phrase'],
-                              req.form['letters'],
-                              req.remote_addr,
-                              req.user_agent.browser,
-                              res, ))
+@app.route('/login')
+def do_login() -> str:
+    session['logged_in'] = True
+    return 'You are now logged in.'
+
+
+@app.route('/logout')
+def do_logout() -> str:
+    session.pop('logged_in')
+    return 'You are now logged out.'
 
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
+    @copy_current_request_context
+    def log_request(req: 'flask_request', res: str) -> None:
+        with UseDatabase(app.config['dbconfig']) as cursor:
+            _SQL = """insert into log
+                        (phrase, letters, ip, browser_string, result)
+                        values(%s, %s, %s, %s, %s)"""
+            cursor.execute(_SQL, (req.form['phrase'],
+                                  req.form['letters'],
+                                  req.remote_addr,
+                                  req.user_agent.browser,
+                                  res, ))
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your result'
     results = str(search4letters(phrase, letters))
-    log_request(request, results)
+    try:
+        t = Thread(target=log_request, args=(request, results))
+        t.start()
+    except Exception as err:
+        print('Произошла ошибка: ', str(err))
+
     return render_template('result.html',
                            the_phrase=phrase,
                            the_letters=letters,
@@ -48,18 +65,27 @@ def entry_page() -> 'html':
 @app.route('/viewlog')
 @check_logged_in
 def view_log() -> 'html':
-    contents = []
-    with UseDatabase(app.config['dbconfig']) as cursor:
-        _SQL = """select phrase, letters, ip, browser_string, result from log"""
-        cursor.execute(_SQL)
-        contents = cursor.fetchall()
-    titles = ('Phrase', 'Letters',
-              'Remote_addr', 'User_agent', 'Result')
-    return render_template('viewlog.html',
-                           the_title='View log',
-                           the_row_titles=titles,
-                           the_data=contents
-                           )
+    try:
+        with UseDatabase(app.config['dbconfig']) as cursor:
+            _SQL = """select phrase, letters, ip, browser_string, result from log"""
+            cursor.execute(_SQL)
+            contents = cursor.fetchall()
+        titles = ('Phrase', 'Letters',
+                  'Remote_addr', 'User_agent', 'Result')
+        return render_template('viewlog.html',
+                               the_title='View log',
+                               the_row_titles=titles,
+                               the_data=contents
+                               )
+    except ConnectionError as err:
+        print('Is your database switched on? Error:', str(err))
+    except CredentialsError as err:
+        print('User-id/Password issues. Error:', str(err))
+    except SQLError as err:
+        print('Is your query correct? Error:', str(err))
+    except Exception as err:
+        print('Something went wrong:', str(err))
+    return 'Error'
 
 
 app.secret_key = 'mysecretKey'
